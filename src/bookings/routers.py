6c4c7de941ee69b4ab900ee7c4a6fb3f886c2e81
sator_nr <sator_nr@mail.ru>
2023-11-06 05:04:48 +0400

    Basic auth, jwt, cookies

--- src/bookings/routers.py
+++ src/bookings/routers.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy import select
+from starlette.requests import Request
 
 from src.bookings.dao import BookingDAO
 from src.bookings.models import Bookings
 from src.bookings.schemas import SBooking
 from src.database import async_session_maker
+from src.users.models import Users
+from src.users.dependencies import get_current_user
 
 router = APIRouter(
     prefix='/bookings',
@@ -13,8 +16,8 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
 
 
 
