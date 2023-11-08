1c3a84b4a8437e7ee8206dcfad08fbc6c6dc5ea5
sator_nr <sator_nr@mail.ru>
2023-11-08 04:29:38 +0400

    Redis, Celery, minor changes

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -28,7 +28,7 @@ def send_booking_confirmation_email(
     booking: dict,
     email_to: EmailStr
 ):
-    email_to_mock = 'sator_nr@mail.ru'  # Заглушка, в базе нет тестовых
+    email_to_mock = ''
     msg_content = create_booking_confirmation_template(booking, email_to_mock)
     with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
         server.login(settings.SMTP_USER, settings.SMTP_PASS)
